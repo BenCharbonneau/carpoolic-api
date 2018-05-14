8ee448ac3da44210a57cdf8f96027c41dfacdb5f
agraham <agraham0925@gmail.com>
2018-05-14 09:47:09 -0500

    can delete (or cancel) rides

--- controllers/RideController.rb
+++ controllers/RideController.rb
@@ -36,5 +36,36 @@ class RideController < ApplicationController
 		}.to_json
 	end
 
+	post '/' do
+		pp params
+
+		@ride = Ride.new
+		@ride.name = @payload[:name]
+		@ride.pickup = @payload[:pickup]
+		@ride.destination = @payload[:destination]
+		@ride.pickup_date = @payload[:pickup_date]
+		@ride.pickup_time = @payload[:pickup_time]
+		@ride.driver_user_id = @payload[:driver_user_id]
+		@ride.passenger_slots = @payload[:passenger_slots]
+		@ride.save
+
+		{
+			success: true,
+			message: "you added ride \##{@ride.id}",
+			added_ride: @ride
+		}.to_json
+	end
+
+	delete '/:id' do
+		@ride = Ride.find params[:id]
+		@ride.destroy
+
+		{
+			success: true,
+			message: "you deleted ride id \##{@ride.id}",
+			deleted_ride: @ride
+		}.to_json
+	end
+
 
 end
