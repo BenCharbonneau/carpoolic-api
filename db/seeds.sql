78e381bddbf8d9612d90d07fbd3cf29d9f47a36c
agraham <agraham0925@gmail.com>
2018-05-15 12:47:08 -0500

    fixed seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,7 +8,7 @@ INSERT INTO users (username, password, email) VALUES ('noemi', 1234, 'jim@email.
 INSERT INTO rides (name, pickup, destination, pickup_date, pickup_time, driver_user_id, passenger_slots) VALUES ('going to solider field', 'some place', 'solider field', '2018-05-11', '09:30:00', 1, 3);
 INSERT INTO rides (name, pickup, destination, pickup_date, pickup_time, driver_user_id, passenger_slots) VALUES ('going to united center', 'some where', 'united center', '2018-05-15', '10:00:00', 1, 2);
 INSERT INTO rides (name, pickup, destination, pickup_date, pickup_time, driver_user_id, passenger_slots) VALUES ('going to see da bears', 'some place there', 'solider field', '2018-05-19', '13:30:00', 1, 4);
-INSERT INTO rides (name, pickup, destination, pickup_date, pickup_time, driver_user_id, passenger_slots) VALUES ('going to mccormick place', 'some here', 'mccormick place', '2018-05-11', '19:00:00', 2, 4);
+INSERT INTO rides (name, pickup, destination, pickup_date, pickup_time, driver_user_id, passenger_slots) VALUES ('going to mccormick place', 'some here', 'mccormick place', '2018-05-11', '19:00:00', 3, 4);
 INSERT INTO rides (name, pickup, destination, pickup_date, pickup_time, driver_user_id, passenger_slots) VALUES ('navy pier rulez', 'some place', 'navy pier', '2018-05-13', '07:30:00', 2, 1);
 INSERT INTO rides (name, pickup, destination, pickup_date, pickup_time, driver_user_id, passenger_slots) VALUES ('daaaa bears', 'some place near', 'solider field', '2018-05-18', '08:00:00', 2, 2);
 INSERT INTO rides (name, pickup, destination, pickup_date, pickup_time, driver_user_id, passenger_slots) VALUES ('dat pier', 'some place far', 'navy pier', '2018-05-21', '22:30:00', 3, 4);
